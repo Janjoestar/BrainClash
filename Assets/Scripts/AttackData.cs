46b8f4fd3a3d3e6007b1df51ab5eeba3de84f41a
JanJoestar <janishgandhi.13@gmail.com>
2025-03-08 10:57:21 +0100

    Diffrent Scenes, Scripts
    Gamemanager, Quizmanager, Battlemanager Scripts
    
    Players can use the buzzer, Answer the Quesitons(kind of), 4 diffrent attacks with placeholder "Animations", Player can attack.

---
+++ Assets/Scripts/AttackData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class AttackData
+{
+    public string attackName;
+    public int damage;
+    public string description;
+    public Color effectColor = Color.red;
+}
